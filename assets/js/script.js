81ba86d431bfa52aaecf27b094833982028dddd4
SaiTeja69 <54733232+SaiTeja69@users.noreply.github.com>
2020-04-27 12:38:48 +0530

    Trial 1 at changing handwriting styles
    Status :-1: not working!

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,16 +1,5 @@
 window.onload = function () {
-    var fileupload = document.getElementById("pageUploader");
-    var button = document.getElementById("btnFileUpload");
-    button.onclick = function () {
-        fileupload.click();
-    };
-    fileupload.onchange = function () {
-        var reader = new FileReader();
-        reader.readAsDataURL(fileupload.files[0]);
-        reader.onload = function (e) {
-            img = loadImage(e.target.result);
-        }
-    };
+
     loadDarkModeFromLocalStorage();
     darkLightToggle()
 };
