15055f1ee5af208e307864e0698dfaf89da99d5c
tusharpandey13 <tusharpandey13@gmail.com>
2020-06-04 16:25:50 +0530

    16:25:50

--- temp/name_change.py
+++ temp/name_change.py
@@ -9,6 +9,6 @@ count = 65
 for i in range(27, 52):
     os.system("rename " + str(i) + ".jpg" + " " + str(count) + ".jpg")
     count += 1 '''
-count =91
-for i in range(50,57):
-	os.system("rename " + str(i) + ".jpg" + " " + str(i-4) + ".jpg")
+count = 91
+for i in range(50, 57):
+    os.system("rename " + str(i) + ".jpg" + " " + str(i - 4) + ".jpg")
