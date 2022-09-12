c08410ae61992dcbab6e40a2bb91f2bff61dbc13
Varun-Kumar-Singh <25012003.vs@gmail.com>
2022-09-12 14:27:12 +0530

    my text added

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get("/",function(req,res){
   res.sendFile(__dirname + "/index.html");
 });
 
-app.listen(3000,function(){
+app.listen(process.env.PORT || 3000,function(){
   console.log("Port 3000 started");
 });
